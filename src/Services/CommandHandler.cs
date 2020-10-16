296d0bbfff4ea22d1789fc76d0b1c5c9ddf4e7e1
lily liao <40707243+0x10fox@users.noreply.github.com>
2020-10-15 22:53:29 -0400

    remove unused code
    made that command handler 50% better by removing all my broken shit from previous versions of the handler

--- src/Services/CommandHandler.cs
+++ src/Services/CommandHandler.cs
@@ -8,47 +8,7 @@ namespace crackdotnet
 {
     public class CommandHandler
     {
-        /**
-        private readonly DiscordSocketClient _discord;
-        private readonly CommandService _commands;
-        private readonly IConfigurationRoot _config;
-        private readonly IServiceProvider _provider;
-
-        // DiscordSocketClient, CommandService, IConfigurationRoot, and IServiceProvider are injected automatically from the IServiceProvider
-        public CommandHandler(
-            DiscordSocketClient discord,
-            CommandService commands,
-            IConfigurationRoot config,
-            IServiceProvider provider)
-        {
-            _discord = discord;
-            _commands = commands;
-            _config = config;
-            _provider = provider;
-
-            _discord.MessageReceived += OnMessageReceivedAsync;
-        }
         
-        private async Task OnMessageReceivedAsync(SocketMessage s)
-        {
-            var msg = s as SocketUserMessage;     // Ensure the message is from a user/bot
-            if (msg == null) return;
-            if (msg.Author.Id == _discord.CurrentUser.Id) return;     // Ignore self when checking commands
-            
-            var context = new SocketCommandContext(_discord, msg);     // Create the command context
-
-            int argPos = 0;     // Check if the message has a valid command prefix
-            if (msg.HasStringPrefix(_config["prefix"], ref argPos) || msg.HasMentionPrefix(_discord.CurrentUser, ref argPos))
-            {
-                var result = await _commands.ExecuteAsync(context, argPos, _provider);     // Execute the command
-
-                if (!result.IsSuccess) Console.WriteLine("something sucks");    // If not successful, reply with the error.
-                    //await context.Channel.SendMessageAsync(result.ToString());
-                
-            }
-        
-        }
-        **/
         //private readonly ILogger<CommandHandlingService> _logger;
         private readonly CommandService _commandService;
         private readonly DiscordSocketClient _discord;
@@ -68,17 +28,6 @@ namespace crackdotnet
             _discord.MessageReceived += OnMessageReceivedAsync;
         }
 
-        public void Start()
-        {
-            _discord.MessageReceived += OnMessageReceivedAsync;
-            //_logger.LogInformation("Started");
-        }
-
-        public void Stop()
-        {
-            _discord.MessageReceived -= OnMessageReceivedAsync;
-            //_logger.LogInformation("Stopped");
-        }
 
         private async Task OnMessageReceivedAsync(SocketMessage s)
         {
